923ac2f9905a223f8fd9319eed52196f44ba88af
Denis Olsem <dols3m@gmail.com>
2017-12-08 23:14:57 -0500

    Aldo rewrote preprocessing.

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,7 @@
 import wave, struct, csv, time
-import pickle
+import pickle, numpy
 
-root_path = "./db/SantaBarbara/"
+root_path = ""
 #this parses the transcript into a list of dictionaries for each time slice
 def timeParse(i):
     with open(root_path+"TRN/SBC%03d.trn" % (i)) as f:
@@ -9,13 +9,21 @@ def timeParse(i):
         result = []
         for line in f.readlines():
             try:
-                time, n, words = line.split("\t")
+                foo = line.split("\t")
+                if len(foo)==4:
+                    start, end = map(float, foo[0:2])
+                    name = foo[2].strip().strip(":") if not (foo[2].isspace() or len(foo[2])==0) else name
+                elif len(foo) == 3:
+                    start, end = map(float, foo[0].split())
+                    name = foo[1].strip().strip(":") if not (foo[1].isspace() or len(foo[1])==0) else name
+                else:
+                    print "formatting transcript error: ",i, foo
+                    continue
+#                 print foo, start, end, name
+                result +=[{"start":start, "end":end, "name":name.upper()}]
             except ValueError:
                 print "Value Error: %s" % (line.strip())
                 continue
-            start, end = map(float, time.split())
-            name = n.strip().strip(":") if not n.isspace() else name
-            result+=[{"start": start, "end": end, "name": name.upper(), "words": words.rstrip("\n")}]
         return result
 
 #this parses the metadata files with the given headers and returns them as a dictionary
@@ -35,7 +43,7 @@ def metaParse(i):
 #this parses the audio .wav files with the "wave" library and the "struct" library to unpack the byte stream
 #pulls all samples from in the selection size, storing it as a list of tuples from the channels
 def audioParseSelection(i, start, end):
-    w = wave.open(root_path+"wav/SBC%03d.wav" % (i), 'r')
+    w = wave.open(root_path+"wave/SBC%03d.wav" % (i), 'r')
     w.setpos(int(start * w.getframerate()))
     try:
         raw = [struct.unpack("%dh" % (w.getnchannels()), w.readframes(1)) for _ in range(int(w.getframerate() * (end - start)))]
@@ -43,33 +51,62 @@ def audioParseSelection(i, start, end):
         return {"left": [x[0] for x in raw], "right": [x[0] for x in raw]}, w.getframerate()
     except Exception as e:
         print "%s at %d from %.2f to %.2f" % (str(e), i, start, end)
-        return [],0
-
+        return [],0 
+    
 #this combines all three data sources together into individual dictionary elements
 #based on the times slices, it then pulls in all available metadata (matching the names)
 #and audio values from the wave (using the start and end seconds)
 def combine (i, time, meta):
-    #print "%d, %s: %f to %f" % (i, time.get("name"), time.get("start"), time.get("end"))
+#     print "%d, %s: %f to %f" % (i, time.get("name"), time.get("start"), time.get("end"))
     audio, freq = audioParseSelection(i, time.get("start"), time.get("end"))
     if time.get("name") in meta.keys() and len(audio) > 0:
         time.update(meta.get(time.get("name")))
+        time.update({"conv_id" : i, "freq": freq})
+#         print time, str(audio)[:100]
         time.update({"wav" : audio, "nframes" : len(audio)})
-        time.update({"scene_id" : i, "freq": freq})
         return time
     else:
         print "data not found:", time.get("name"), time.get("start"), time.get("end"), len(audio)
-
-def pickleData(meta, r):
+  
+def pickleData(meta, r, step):
     for i in r:
-        f = open(root_path + "sb%d.p" % (i), "wb")
         times = timeParse(i)
         print "i: %d, total: %d" % (i, len(times))
-        pickle.dump([combine(i, dict(time), meta) for time in times], f)
-        f.close()
+        data = [combine(i, dict(time), meta) for time in times]
+        clean(i, step, data)
+
+def test(foo):
+    return numpy.mean(map(abs, foo)) >500
+
+def clean(i, step, original):
+    result = []
+    for x in original:
+        if x == None:
+            continue
+        start, end, freq, audio= x['start'], x['end'], x['freq'], x['wav']
+        newAudio = {'left':[], 'right':[]}
+        for t in range(0, int((end - start) * freq), int(step * freq)):
+            tEnd = int(min(t+step*freq, (end - start) * freq))
+            audioLclip, audioRclip = list(audio['left'][t:tEnd]), list(audio['right'][t:tEnd])
+            if test(audioLclip) and test(audioRclip):
+                newAudio['left'] +=audioLclip
+                newAudio['right'] += audioRclip
+                
+        if len(newAudio['left']) > step*freq /2:
+            newX = dict(x)
+            newX.update({'wav': newAudio})
+            result+=[newX]
+            
+    print "%d -> %d" % (len(original), len(result))
+    fOut = open("Output/sbClean%d.p" % (i), 'wb')
+    pickle.dump(result, fOut)
+    fOut.close()
 
 ranges = [range(1,15), range(15,31), range(31, 47), range(47, 61)]
-
-for x in [1, 2, 3, 4]:
-    print("Processing metadata #{}, conversations {}...".format(x, ranges[x-1]))
-    pickleData(metaParse(x), ranges[x-1])
-    print "Done.\n"
+ 
+seconds = 1.0
+x = 1 # 1-4 available
+ 
+print "start"
+pickleData(metaParse(x), ranges[x-1], seconds)
+print "all done"
